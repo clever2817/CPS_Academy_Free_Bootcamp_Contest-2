28a3503d98f6d5ab9c323fa8dc7d9dbc4c967d56
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-24 17:56:37 +0600

    Problem C Solved

---
+++ C-2UP3DOWN.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cin >> x >> y;
+
+    if(x < y)
+    {
+        if((y-x) <= 2)
+        {
+            cout << "Yes\n";
+        }
+
+        else
+        {
+            cout << "No\n";
+        }
+    }
+
+    else if(x > y)
+    {
+        if((x-y) <= 3)
+        {
+            cout << "Yes\n";
+        }
+
+        else
+        {
+            cout << "No\n";
+        }
+    }
+
+    return 0;
+}
